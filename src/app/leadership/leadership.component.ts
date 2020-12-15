d3ebdd3bf83190eae4d8e4c058edb6156fadebda
Kera Z. Watkins <kzw@acm.org>
2020-12-14 23:39:23 -0500

    updated leadership table

--- src/app/leadership/leadership.component.ts
+++ src/app/leadership/leadership.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./leadership.component.scss']
 })
 export class LeadershipComponent implements OnInit {
+  public data: any[];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.data = [
+      {
+        name: "Brian Hinkle",
+        phone: "301-980-4712",
+        email: "BDHink@gmail.com"
+      },
+      {
+        name: "Lauren Hinkle",
+        phone: "301-980-4815",
+        email: "RLNicUNurse@gmail.com"
+      },
+      {
+        name: "Doug Phebus",
+        phone: "937-554-6001",
+      },
+      {
+        name: "Michelle Phebus",
+        phone: "937-305-3081",
+      }
+    ];
   }
 
 }
